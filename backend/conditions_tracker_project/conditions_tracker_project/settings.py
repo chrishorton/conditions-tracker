fbc86b2d22a102f199981286521c3c3210fd5b88
chrishorton <chris.horton@sbcglobal.net>
2021-03-21 20:18:00 +0100

    os

--- backend/conditions_tracker_project/conditions_tracker_project/settings.py
+++ backend/conditions_tracker_project/conditions_tracker_project/settings.py
@@ -90,10 +90,9 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 CORS_ORIGIN_WHITELIST = [
