7d31e9a3b7c53c20d069c15da881ba8ed39bd365
Chris Horton <12613672+chrishorton@users.noreply.github.com>
2022-01-12 21:56:47 +0100

    Update urls.py

--- backend/conditions_tracker_project/conditions_tracker_project/urls.py
+++ backend/conditions_tracker_project/conditions_tracker_project/urls.py
@@ -22,6 +22,6 @@ from conditions_tracker_app.schema import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True,schema=schema))),
-    # path('patients/<int:id>/', views.),
-    # path('patients/<int:id>/conditions')
+    path('patients/<int:id>/', views.),
+    path('patients/<int:id>/conditions')
 ]
